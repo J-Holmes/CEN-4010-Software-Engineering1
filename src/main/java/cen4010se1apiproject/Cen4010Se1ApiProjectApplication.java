3bc7a26db9aa277be6bcaa68c9b251ca088e0885
jholm044 <jholm044@fiu.edu>
2022-12-08 12:15:30 -0500

    Initial commit

---
+++ src/main/java/cen4010se1apiproject/Cen4010Se1ApiProjectApplication.java
@@ -0,0 +1,13 @@
+package cen4010se1apiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cen4010Se1ApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cen4010Se1ApiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cen4010se1apiproject/Cen4010Se1ApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cen4010se1apiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cen4010Se1ApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
